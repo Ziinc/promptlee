51efb08b4430ed5a50527c535923d10a802493eb
TzeYiing <ty@tzeyiing.com>
2023-09-22 04:34:32 +0800

    chore: remove unused code

--- src/interfaces/PromptResult.tsx
+++ src/interfaces/PromptResult.tsx
@@ -11,35 +11,6 @@ const PromptResult: React.FC<Props> = ({ result }) => {
   const choice = result.choices[0];
   return (
     <Typography variant="body1">{choice.message?.content}</Typography>
-    // {/* {result.usage && (
-    //   <Popover
-    //     content={
-    //       <Descriptions
-    //         className="max-w-sm"
-    //         title="Cost Estimation"
-    //         size="small"
-    //         layout="vertical"
-    //         column={1}
-    //         colon={false}
-    //       >
-    //         <Descriptions.Item label="Prompt">
-    //           {result.usage.prompt_tokens} tokens
-    //         </Descriptions.Item>
-    //         <Descriptions.Item label="Completion">
-    //           {result.usage.completion_tokens} tokens
-    //         </Descriptions.Item>
-    //         <Descriptions.Item label="Estimated Cost ($0.002 /1k tokens)">
-    //           ${((result.usage?.total_tokens * 0.002) / 1000).toPrecision(4)}
-    //         </Descriptions.Item>
-    //       </Descriptions>
-    //     }
-    //   >
-    //     <Tag>
-    //       {result.usage?.total_tokens} tokens ≈ $
-    //       {((result.usage?.total_tokens * 0.002) / 1000).toPrecision(2)}
-    //     </Tag>
-    //   </Popover>
-    // )} */}
   );
 };
 export default PromptResult;
