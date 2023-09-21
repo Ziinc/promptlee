43dfac625a262963fdf3418d715b8f10893ba411
TzeYiing <ty@tzeyiing.com>
2023-09-22 04:34:41 +0800

    chore: formatting

--- src/interfaces/PromptResult.tsx
+++ src/interfaces/PromptResult.tsx
@@ -9,8 +9,6 @@ const PromptResult: React.FC<Props> = ({ result }) => {
   // don't support multi choice yet
   if (result.choices.length > 1) return null;
   const choice = result.choices[0];
-  return (
-    <Typography variant="body1">{choice.message?.content}</Typography>
-  );
+  return <Typography variant="body1">{choice.message?.content}</Typography>;
 };
 export default PromptResult;
